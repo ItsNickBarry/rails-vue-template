ca3eb4130f3f3b01441e34085b0b9bbcf58b2f40
Nick Barry <itsnickbarry@protonmail.ch>
2019-11-09 20:11:13 -0600

    use guard, minitest, simplecov, and related gems

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 
   gem 'foreman'
+  gem 'guard'
+  gem 'guard-minitest'
 end
 
 group :test do
@@ -51,6 +53,13 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+
+  gem 'minitest-rails'
+  gem 'minitest-reporters'
+  gem 'minitest-reporters-ordered_spec_reporter'
+  gem 'minitest_autoskip'
+  gem 'simplecov', require: false
+  gem 'rails-controller-testing'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,17 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start :rails unless ENV['NO_COVERAGE']
+
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/spec'
+require 'minitest/reporters'
+require 'minitest/reporters/ordered_spec_reporter'
+require 'minitest_autoskip'
+
+Minitest::Reporters.use! Minitest::Reporters::OrderedSpecReporter.new
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,4 +21,13 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  # include devise helpers for all controller and integration tests
+  include Devise::Test::IntegrationHelpers
+
+  def json_response
+    @json_response ||= ActiveSupport::JSON.decode response.body
+  end
 end
