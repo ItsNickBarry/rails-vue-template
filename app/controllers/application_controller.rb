0d6391feb4397e01186b2928fb64105b7124687c
Nick Barry <itsnickbarry@protonmail.ch>
2019-11-11 16:46:49 -0600

    add error-response rendering methods to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   private
 
+    def render_forbidden!
+      render json: ["Your account does not have access to the requested resource."], status: 403
+    end
+
+    def render_unprocessable_entity! entity
+      render json: entity.errors.full_messages, status: 422
+    end
+
     def layout_by_resource
       if devise_controller?
         'devise'
